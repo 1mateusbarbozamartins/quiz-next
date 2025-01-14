4d1845129907f03bf8330006258413688995a548
Mateus <mateusbarbozamartins@gmail.com>
2025-01-14 10:27:00 -0300

    Reparando erro de tipo em Temporizador

--- components/Temporizador.tsx
+++ components/Temporizador.tsx
@@ -16,11 +16,9 @@ export default function Temporizador(props: TemporizadorProps) {
                 size={120}
                 isPlaying
                 onComplete={props.tempoEsgotado}
-                colors={[
-                    ['#BCE596', 0.33],
-                    ['#F7B801', 0.33],
-                    ['#ED827A', 0.33],
-                ]}>
+                colors={['#BCE596', '#F7B801', '#ED827A']}
+                colorsTime={[props.duracao * 0.33, props.duracao * 0.33, props.duracao * 0.34]}
+            >
                 {({ remainingTime }) => remainingTime}
             </CountdownCircleTimer>
         </div>
